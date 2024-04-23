e34c15df304c9c5004a0ba9495abdc2158c2ddc4
Gab-Serrano <gab.serrano@duocuc.cl>
2024-04-23 00:53:27 -0400

    modelo finalizado

--- models/recipe.js
+++ models/recipe.js
@@ -21,11 +21,134 @@ const recipeSchema = new Schema({
          maxlength: 255
       },
       recipeData: {
-         dificulty: String,
-         yield: String,
-         preparationTime: String,
-         cockingTime: String,
-         required: true
-      }
-
+         dificulty: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 255
+         },
+         yield: {
+            type: Number,
+            required: true,
+         },
+         preparationTime: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 255
+         },
+         cockingTime: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 255
+         },
+         required: true,
+      },
+      specialNeeds: {
+         type: [String],
+         required: true,
+      },
+      nutritionalInfo: {
+         totalKcal: {
+            type: Number,
+            required: true,
+         },
+         fat: {
+            kcal: {
+               type: Number,
+               required: true,
+            },
+            gr: {
+               type: Number,
+               required: true,
+            },
+            porcentage: {
+               type: Number,
+               required: true,
+            },
+            required: true,
+         },
+         carbohydrates: {
+            kcal: {
+               type: Number,
+               required: true,
+            },
+            gr: {
+               type: Number,
+               required: true,
+            },
+            porcentage: {
+               type: Number,
+               required: true,
+            },
+            required: true,
+         },
+         protein: {
+            kcal: {
+               type: Number,
+               required: true,
+            },
+            gr: {
+               type: Number,
+               required: true,
+            },
+            porcentage: {
+               type: Number,
+               required: true,
+            },
+            required: true,
+         },
+         additionalInfo: {
+            sugars: {
+               type: Number,
+               required: true,
+            },
+            fiber: {
+               type: Number,
+               required: true,
+            },
+            saturatedFats: {
+               type: Number,
+               required: true,
+            },
+            salt: {
+               type: Number,
+               required: true,
+            },
+            required: true,
+         },
+         required: true,
+      },
+      yieldPerAge: {
+         adult: {
+            type: String,
+            required: true,
+         },
+         threeToEight: {
+            type: String,
+            required: true,
+         },
+         nineToTwelve: {
+            type: String,
+            required: true,
+         },
+         teen: {
+            type: String,
+            required: true,
+         },
+         required: false,
+      },
+      ingredientLines: {
+         type: [String],
+         required: true,
+      },
+      directions: {
+         type: [String],
+         required: true,
+      },
+      chefTips: {
+         type: [String],
+         required: false,
+      },
    });
